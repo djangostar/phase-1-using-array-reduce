027dd670e0748ff266f5c3c19053b14b080da0ba
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2021-12-22 17:06:24 -0800

    commit tree 18807930fdd6ff62498eb96463a5dbe4456045d5

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+//const totalBatteries = batteryBatches.reduce(function(accum, value) {
+//    return accum + value
+//})
+
+
+
+//Refractored into an arrow function
+const totalBatteries = batteryBatches.reduce((accum, value) => accum + value);
